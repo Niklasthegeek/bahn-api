3ead00de11d3f318fb3ee05201dc9ac9691419d2
Niklasthegeek <polten44@gmail.com>
2023-04-29 02:49:10 +0200

    Openstreetmap Einbindung

--- index.php
+++ index.php
@@ -18,6 +18,8 @@ require 'backend.php';
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.css" />
     <script type="module">
       import Autocomplete from "./autocomplete.min.js";
       const opts = {
@@ -51,8 +53,6 @@ require 'backend.php';
                   placeholder="Type something"
                   required
                 />
-                <!--<input type="text" class="form-control" id="evaNo" name="evaNo" value="<?php echo isset($_GET['evaNo']) ? $_GET['evaNo'] : ''; ?>"
-                    placeholder="Enter EvaNo" required>-->
             </div>
             <div class="form-group col-12">
                 <label for="date">Datum:</label>
@@ -104,7 +104,7 @@ require 'backend.php';
             <button type="submit" class="btn btn-primary">Get Timetable</button>
         </form>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-5">
         <?php
         
         if (isset($_GET['bahnhof']) && isset($_GET['date']) && isset($_GET['hour'])&& isset($_GET['mode'])) {
@@ -127,7 +127,7 @@ require 'backend.php';
                 $th_2 = "Zielbahnhof";
             }
         ?>
-        <div class="col-md-9">
+        <div >
             <table class="table">
               <tbody>
                 <tr>
@@ -147,7 +147,26 @@ require 'backend.php';
             </table>        
         </div>
         </div>
+        <div id="mapid" class="col-md-4">
+        <script>
+        // OpenStreetMap-Karte erstellen
+        var mymap = L.map('mapid').setView([<?php echo getStationDetails($evaNo, 'result.0.evaNumbers.0.geographicCoordinates.coordinates.1') ?>, <?php echo getStationDetails($evaNo, 'result.0.evaNumbers.0.geographicCoordinates.coordinates.0') ?>], 13);       
 
+        // Tile-Layer von OpenStreetMap hinzufügen
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1
+        }).addTo(mymap);        
+
+        // Marker hinzufügen
+        L.marker([<?php echo getStationDetails($evaNo, 'result.0.evaNumbers.0.geographicCoordinates.coordinates.1') ?>, <?php echo getStationDetails($evaNo, 'result.0.evaNumbers.0.geographicCoordinates.coordinates.0') ?>]).addTo(mymap);
+        </script>
+        </div>
+        </div>
         <br>
         <div id="timetable-results"><br>
 
@@ -169,7 +188,17 @@ require 'backend.php';
               <tr <?php if ($row['filter'] == "F") { ?>style="background-color: #ffcccc;"<?php } elseif ($row['filter'] == "N") { ?>style="background-color: #e6ffe6;"<?php } elseif ($row['filter'] == "S") { ?>style="background-color: #ccebff;"<?php } ?>>
                 <td><?php echo $row['time']; ?> Uhr</td>
                 <td><?php echo $row['gleis']; ?></td>
-                <td><?php echo $row['elevator']; ?></td>
+                <td><?php if ($row['elevator'] == "TRUE") { ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16">
+                      <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                      <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
+                    </svg>
+                  <?php } elseif($row['elevator'] == "FALSE"){ ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16">
+                      <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                    </svg>
+                  <?php } ?></td>
                 <td><?php echo $row['bhf_0']; ?></td>
                 <td><?php echo $row['bhf_end']; ?></td>
                 <td><?php echo $row['category']; ?></td>
