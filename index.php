f809a7f80181db0b72f5ce3ceb868363f57efff9
Niklasthegeek <polten44@gmail.com>
2023-05-09 22:54:28 +0200

    Footer mit Links zum Projekt eingefügt

--- index.php
+++ index.php
@@ -37,8 +37,8 @@ date_default_timezone_set('Europe/Berlin');
       }
     </script>
 </head>
-<body>
-    <div class="container-fluid">
+<body style="min-height: 100%; display: flex; flex-direction: column;">
+    <div class="container-fluid" style="flex-grow: 1; ">
         <h1>Bahn-Infotafel</h1>
         <div class="row">
         <div class="col-md-3">
@@ -226,6 +226,19 @@ date_default_timezone_set('Europe/Berlin');
         <?php
         }
         ?>
+    </div>
+    <div class="container-fluid">
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-3 d-flex align-items-center">
+          <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 <a href="https://github.com/Niklasthegeek" target="_blank" class="text-decoration-none text-body-secondary">Niklasthegeek</a>, LICENSE MIT</span>
+        </div>
+        <div class="col-md-6 d-flex justify-content-center">
+        Eine Implemetation verschiedener DB APIs. Code unter:&nbsp;<a href="https://github.com/Niklasthegeek/bahn-api" class="text-decoration-none text-primary">https://github.com/Niklasthegeek/bahn-api</a>
+        </div>
+        <ul class="nav col-md-3 justify-content-end list-unstyled d-flex">
+          <li class="ms-3"><a class="text-body-secondary" href="https://github.com/Niklasthegeek/bahn-api"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg></a></li>
+        </ul>
+      </footer>
     </div>
 </body>
 </html>
